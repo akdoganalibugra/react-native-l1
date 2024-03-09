eeec5b01e869b1f813de01c9b6ce69e25a5e8444
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-03-09 13:20:16 +0300

    welcome screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,41 +1,7 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", // main
-        alignItems: "center", // secondary
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          height: 100,
-          width: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          height: 100,
-          width: 100,
-          top: 20,
-          left: 20,
-          position: "relative",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          height: 100,
-          width: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
